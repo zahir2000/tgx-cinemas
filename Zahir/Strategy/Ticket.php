d75cc726b7a9165fbb245d074e8271394cf3a53c
Zahiriddin Rustamov <57207346+zahir2000@users.noreply.github.com>
2020-08-18 01:37:40 +0800

    Add files via upload

--- Zahir/Strategy/Ticket.php
+++ Zahir/Strategy/Ticket.php
@@ -10,9 +10,29 @@ class Ticket {
     private $price;
     private $type;
 
-    function __construct($price = "", $type = "") {
+    function __construct($price, $type) {
         $this->price = $price;
         $this->type = $type;
+    }
+
+    function getPrice() {
+        return $this->price;
+    }
+
+    function getType() {
+        return $this->type;
+    }
+
+    function setPrice($price): void {
+        $this->price = $price;
+    }
+
+    function setType($type): void {
+        $this->type = $type;
+    }
+    
+    public function cost(){
+        return $this->type->cost($this->price);
     }
 
 }
