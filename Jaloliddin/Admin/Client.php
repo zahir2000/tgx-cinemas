de871c8c95b3858e15704d3cd1b58d6c9b5ad99f
Jaloliddin <mr.romaboy@gmail.com>
2020-08-23 23:47:24 +0800

    Design Pattern Completed

--- Jaloliddin/Admin/Client.php
+++ Jaloliddin/Admin/Client.php
@@ -19,7 +19,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <title>Admin Login</title>
+        <title>Administrator Login</title>
     </head>
     <body>
         <?php
