19f89488df7f247d3e8e9e8aab2a715052e064c5
zahir2000 <zakisher@gmail.com>
2020-08-24 17:20:41 +0800

    Session for login

--- Database/BookingConnection.php
+++ Database/BookingConnection.php
@@ -308,6 +308,21 @@ class BookingConnection {
         }
     }
 
+    public function getUserToken($username) {
+        $query = "SELECT token FROM usertoken WHERE username = ?";
+        $stmt = $this->db->getDb()->prepare($query);
+        $stmt->bindParam(1, $username, PDO::PARAM_STR);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $result = null;
+        }
+        
+        return $result;
+    }
+
     public function updateToken($username, $userToken) {
         $tokenQuery = "SELECT * FROM usertoken WHERE username = ?";
         $stmt = $this->db->getDb()->prepare($tokenQuery);
